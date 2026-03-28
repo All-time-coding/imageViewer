28e93174706282f4bfef57c4d80c999c79dfe1bb
Ilya Sarevich <ilyasuarezo7@mail.ru>
2026-03-28 18:41:15 +0300

    register GalleryView ViewManager stub on Android for codegen

--- android/src/main/java/com/imageviewer/ImageViewModulePackage.kt
+++ android/src/main/java/com/imageviewer/ImageViewModulePackage.kt
@@ -4,6 +4,7 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
+import com.imageviewer.LoupeImageViewer.ImageViewManager
 
 class ImageViewModulePackage : ReactPackage {
 
@@ -12,7 +13,7 @@ class ImageViewModulePackage : ReactPackage {
     }
 
     override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
-        return emptyList()
+        return listOf(ImageViewManager(reactContext))
     }
 
     override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
